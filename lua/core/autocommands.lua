0dc7a5cf82d3f5e6cf3a407141128b7b6970b276
Anthony J <antonymsj75@gmail.com>
2025-10-23 17:58:55 +0200

    feat: configure LSP formatting on save and update packages to latest version

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -1,13 +1,24 @@
+-- Make UI look cool - highlight yanked text for a moment
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank({ timeout = 200 })
   end,
 })
 
+-- Show the diagnostic for the word under cursor in a floating window
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
   desc = 'Show diagnostic messages in a floating window',
   group = vim.api.nvim_create_augroup("vim_diagnostic", { clear = true }),
   callback = function()
     vim.diagnostic.open_float(nil, { focus = false, scope = 'cursor' })
   end
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  desc = 'Format buffer content using LSP',
+  group = vim.api.nvim_create_augroup("vim_formatting", { clear = true }),
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.lsp.buf.format({ bufnr = bufnr })
+  end,
 })
--- lua/lazyspecs/nvim-telescope.lua
+++ lua/lazyspecs/nvim-telescope.lua
@@ -1,11 +1,11 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    event = { "VeryLazy" },
-    opts = {
-      defaults = {
-        file_ignore_patterns = { "node_modules", ".git/" },
-      },
+  "nvim-telescope/telescope.nvim",
+  event = { "VeryLazy" },
+  opts = {
+    defaults = {
+      file_ignore_patterns = { "node_modules", ".git/" },
     },
-    branch = "master",
-    dependencies = { "nvim-lua/plenary.nvim" }
-  }
+  },
+  branch = "master",
+  dependencies = { "nvim-lua/plenary.nvim" }
+}
