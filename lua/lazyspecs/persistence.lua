c0dcddf1464f96b0914086619406595e856cf577
Anthony J <antonymsj75@gmail.com>
2025-12-14 12:19:19 +0100

    refactor: move persistence.nvim keymaps to the keys in lazyspec

--- lua/lazyspecs/persistence.lua
+++ lua/lazyspecs/persistence.lua
@@ -1,7 +1,24 @@
 return {
-"folke/persistence.nvim",
+  "folke/persistence.nvim",
   lazy = false,
   opts = {
     -- add any custom options here
-  }
+  },
+  keys = function()
+    local keys = {
+      {
+        "<leader>rs",
+        -- load the session for the current directory
+        function() require("persistence").load() end,
+        desc = "[R]estore project session",
+      },
+      {
+        "<leader>rS",
+        -- select a session to load
+        function() require("persistence").select() end,
+        desc = "Select [S]ession to resore",
+      },
+    }
+    return keys
+  end,
 }
